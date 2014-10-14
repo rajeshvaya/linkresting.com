209151ddda89844a0edbc89e5d9ba5c8143e37bf
rajeshvaya <vaya.rajesh@gmail.com>
2014-10-14 09:21:29 +0400

    crispy forms and forms.py for stories add

---
+++ src/linkresting/stories/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+
+from stories.models import Story
+
+
+class StoryForm(ModelForm):
+	helper = FormHelper()
+	helper.form_tag = False
+	helper.error_text_inline = True
+	helper.label_class = "col-md-2"
+	helper.field_class = 'col-md-9'
+
+	class Meta:
+		model = Story
+		exclude = ('moderator',)
