10832e418697c027a933971e576df78c22756d51
rajeshvaya <vaya.rajesh@gmail.com>
2014-10-25 14:24:31 +0400

    manual way of sign up.. need to check on UserCreationForm class

--- src/linkresting/auth/views.py
+++ src/linkresting/auth/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 from forms import SignupForm
 
 def auth(request):
-    
+
     return render(request, 'auth/auth.html', {'SignupForm': SignupForm()})
 
 def signin(request):
@@ -33,10 +34,47 @@ def signin(request):
         return render(request, 'auth/auth.html', {'next': request.GET.get('next', '') })
 
 def signup(request):
-    return render(request, 'auth/auth.html', {SignupForm: SignupForm()})
+    # need to check if the user is already logged in
+    if request.POST:
+        name = request.POST.get('name', False)
+        email = request.POST.get('email', False)
+        password1 = request.POST.get('password1', False)
+        password2 = request.POST.get('password2', False)
+
+        view_data = {
+            'request' : request
+        }
+
+        # check for required fields
+        if(not name or not email or not password1 or not password2):
+            view_data.setdefault('signup_form',{}).setdefault('errors',[]).append("Fields marked * are required")
+            return render(request, 'auth/auth.html', view_data)
+
+        # validaity checks
+        if "@" not in email:
+            view_data.setdefault('signup_form',{}).setdefault('errors',[]).append("Invalid email")
+
+        if password1 != password2:
+            view_data.setdefault('signup_form',{}).setdefault('errors',[]).append("Confirm password doesn't match")
+
+        if(len(view_data.setdefault('signup_form',{}).setdefault('errors',[])) > 0):
+            return render(request, 'auth/auth.html', view_data)
+            
+
+        # check if email already exists
+        try:
+            u = User.objects.get(email=email)
+        except User.DoesNotExist:
+            u = None
+
+        if(u is None):
+            return render(request, 'auth/auth.html', view_data)
+
+        # proceed with creating user
+    
+    return render(request, 'auth/auth.html')
     
 	
-
 def signout(request):
     if request.user.is_authenticated():
         logout(request)
