61a0cf0319e0bd69769c165e7d73dc22775845c5
rajeshvaya <vaya.rajesh@gmail.com>
2014-11-14 15:28:22 +0400

    added STATIC_ROOT for deployment use, no need for development so commentted it out

--- src/linkresting/linkresting/settings/defaults.py
+++ src/linkresting/linkresting/settings/defaults.py
@@ -87,6 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'static'),   
